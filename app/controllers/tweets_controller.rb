ee3ef4ef0ce41f61efa94652bfe67ca2c473de7b
EC2 Default User <ec2-user@ip-172-31-13-126.us-west-2.compute.internal>
2018-07-07 17:09:44 +0000

    ツイート一覧画面にニックネームを表示

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,24 @@
+class TweetsController < ApplicationController
+
+  before_action :move_to_index, except: :index
+
+  def index
+    @tweets = Tweet.page(params[:page]).per(3).order("created_at DESC")
+  end
+
+  def new
+  end
+
+  def create
+    Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+  end
+
+  private
+  def tweet_params
+    params.permit(:image, :text)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+end
