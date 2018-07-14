e2a5acdcdf4a80848122b91a75fd9e2cd18e3ccf
shockdayukio <shockda.yukio@gmail.com>
2018-07-14 11:42:26 +0900

    PhraseモデルのTweet外部キーを削除

--- db/migrate/20180713202415_create_phrases.rb
+++ db/migrate/20180713202415_create_phrases.rb
@@ -3,7 +3,6 @@ class CreatePhrases < ActiveRecord::Migration
     create_table :phrases do |t|
       t.text           :text, null: false
       t.integer        :phrase_type, null: false
-      t.references     :tweet, foreign_key: true
       t.timestamps     null: false
     end
   end
