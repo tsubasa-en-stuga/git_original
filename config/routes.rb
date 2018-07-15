694a3827775e84017dbbf925fd9c1b24a1bf2385
EC2 Default User <ec2-user@ip-172-31-13-126.us-west-2.compute.internal>
2018-07-15 10:58:11 +0000

    親御様用ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'parents/show'
+
   devise_for :users
   root 'tweets#index'
   resources :tweets do
     resources :comments, only: [:create]
   end
   resources :users, only: [:show]
+  resources :parents,only: [:show]
 end
