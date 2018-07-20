6b40fe67bac1194c030e028237d267f8e84d2518
shockdayukio <shockda.yukio@gmail.com>
2018-07-20 17:05:11 +0900

    文章フォームにおける文章部分ごとの表示切り替え機能実装

--- app/assets/javascripts/tweet.js
+++ app/assets/javascripts/tweet.js
@@ -1,9 +1,22 @@
-$(document).ready(function() { hilight_checked_item() });
+$(document).ready(function() { 
+	hilight_checked_item(), 
+	render_phrase_items()
+});
 
+//投稿フォーム用javascript
 function hilight_checked_item(){
   $(".phrase_item").on("click", (function(){
     $(this).siblings().removeClass("checked")
     $(this).addClass("checked")
   }))
 }
-//投稿フォームで、選択された項目を目立たせるためのclassを追加するため
+//クリックされた選択項目を目立たせるためのclassを追加するため
+
+function render_phrase_items(){
+  $(".phrase_type_btn").on("click", (function(){
+       var phrase_type = $(this).attr("id")
+       $(".phrase_items").hide()
+       $(this).siblings("#" + phrase_type).show()
+  }))
+}
+//aタグをクリックすると、タグに応じた文章の選択項目を表示するため
