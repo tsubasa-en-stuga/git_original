98e30a3f94bdfb08e3804f8f62d6f3584689313b
shockdayukio <shockda.yukio@gmail.com>
2018-07-20 18:56:07 +0900

    クリックされた選択項目をフォーム上部に表示するための実装

--- app/assets/javascripts/tweet.js
+++ app/assets/javascripts/tweet.js
@@ -4,13 +4,20 @@ $(document).ready(function() {
 });
 
 //投稿フォーム用javascript
+
 function hilight_checked_item(){
   $(".phrase_item").on("click", (function(){
     $(this).siblings().removeClass("checked")
     $(this).addClass("checked")
+
+    var checked_phrase = $(this).text()
+    var phrase_items_box = $(this).closest(".phrase_items")
+    var phrase_type = phrase_items_box.attr("id")
+    $(".phrase_type_btn#"+phrase_type).text(checked_phrase)
   }))
 }
 //クリックされた選択項目を目立たせるためのclassを追加するため
+//クリックされた選択項目をフォーム上部に表示するため
 
 function render_phrase_items(){
   $(".phrase_type_btn").on("click", (function(){
