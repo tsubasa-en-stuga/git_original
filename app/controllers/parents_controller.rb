85a36cbf79bfe32df591eb2b2ccbef67cba69961
EC2 Default User <ec2-user@ip-172-31-13-126.us-west-2.compute.internal>
2018-07-22 11:05:11 +0000

    親用ページにアバターが表示されるよう修正

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -1,14 +1,16 @@
 class ParentsController < ApplicationController
   def show
+    @user = current_user
   end
 
   def create
     image = current_user.images.create(image_params)
   end
   
+  
   private
   def image_params
-    params.require(:image).permit(:image)
+    params.require(:image).permit(:image, :avatar)
   end
   
 end
