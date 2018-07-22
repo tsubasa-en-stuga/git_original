3b8d4c6894be52165c0e59d152aad55ba0f7bb9b
shockdayukio <shockda.yukio@gmail.com>
2018-07-22 16:50:59 +0900

    日記投稿フォームで、画像一覧表示

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,8 +2,8 @@ class Tweet < ActiveRecord::Base
   belongs_to :user
   has_many :phrases, through: :tweet_phrases
   has_many :tweet_phrases
-  has_many :comments
-  has_one :image           #commentsテーブルとのアソシエーション
+  has_many :comments           #commentsテーブルとのアソシエーション
+  has_one :image
 
   accepts_nested_attributes_for :tweet_phrases
 end
