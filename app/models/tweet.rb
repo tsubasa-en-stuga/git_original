613bd7eb2a27362e78cc8786c182f01a4840aedf
shockdayukio <shockda.yukio@gmail.com>
2018-07-22 15:49:13 +0900

    TweetモデルとImageモデルをアソシエーションで関連付け

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,7 +2,8 @@ class Tweet < ActiveRecord::Base
   belongs_to :user
   has_many :phrases, through: :tweet_phrases
   has_many :tweet_phrases
-  has_many :comments           #commentsテーブルとのアソシエーション
+  has_many :comments
+  has_one :image           #commentsテーブルとのアソシエーション
 
   accepts_nested_attributes_for :tweet_phrases
 end
