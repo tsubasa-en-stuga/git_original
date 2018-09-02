ee7858af5d522f1377cb495d8ddcc5d69fcc36f5
shockdayukio <shockda.yukio@gmail.com>
2018-09-02 14:23:26 +0900

    Tweetsテーブル、Imagesテーブルの主従入れ替え
    ImageがTweetを一つずつ持つテーブル関係、アソシエーションに定義し直した。本来はTweetがImageを持つ定義が関係としては正しいが、
    tweet投稿フォーム、編集フォームビューのヘルパーの書きやすさを重視したためこのような修正を加えた。

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,7 +3,7 @@ class Tweet < ActiveRecord::Base
   has_many :phrases, through: :tweet_phrases
   has_many :tweet_phrases, dependent: :destroy
   has_many :comments           #commentsテーブルとのアソシエーション
-  has_one :image
+  belongs_to :image
 
   accepts_nested_attributes_for :tweet_phrases, allow_destroy: true
 
