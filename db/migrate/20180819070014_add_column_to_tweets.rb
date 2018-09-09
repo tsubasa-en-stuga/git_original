9573eda7491ceefd0afcf4b28f1c6b6b4969436e
EC2 Default User <ec2-user@ip-172-31-13-126.us-west-2.compute.internal>
2018-09-09 07:07:09 +0000

    caunter_cacheのデフォルト値を０に設定

--- db/migrate/20180819070014_add_column_to_tweets.rb
+++ db/migrate/20180819070014_add_column_to_tweets.rb
@@ -1,6 +1,6 @@
 class AddColumnToTweets < ActiveRecord::Migration
   def change
-    add_column :tweets, :likes_count, :integer
-    add_column :tweets, :comments_count, :integer
+    add_column :tweets, :likes_count, :integer, default: 0
+    add_column :tweets, :comments_count, :integer, default: 0
   end
 end
