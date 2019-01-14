13d70173592a0e53e3498cae5cc6c3d2cdcd91da
yukihiro <shockda.yukio@gmail.com>
2019-01-14 22:18:46 +0900

    ユーザ編集時にストロングパラメーターにてnickname, avatarの値を許可

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :avatar])
   end
 end
